343db7fe5b6b9137a7363913b41fc7584b0fa646
John Krumpe <krumpej@Johns-MacBook-Pro-2.local>
2022-05-07 13:55:56 -0400

    Worked on pedestrian spawning

--- Final Project/Assets/Scripts/SpawnPedestrians.cs
+++ Final Project/Assets/Scripts/SpawnPedestrians.cs
@@ -9,10 +9,15 @@ public class SpawnPedestrians : MonoBehaviour
     public GameObject female;
     private int pedestrians = 0;
     private int maxPedestrians = 100;
+    private Bounds block1;
+    private Bounds block2;
+    private Bounds block3;
+    private Bounds block4;
 
     // Start is called before the first frame update
     void Start()
     {
+        block1 = 
         while (pedestrians < maxPedestrians)
         {
             Vector3 pos = RandomPosition();
@@ -33,6 +38,7 @@ public class SpawnPedestrians : MonoBehaviour
             x = Random.Range(-24.0f, 24.0f);
             y = 0;
             z = Random.Range(-13.0f, 13.0f);
+            
         }
     }
 }
